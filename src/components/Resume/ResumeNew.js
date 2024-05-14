d5992d61a1379ac58930f84109835ed662a7ab79
Rudra09 <39452395+Rudra09@users.noreply.github.com>
2024-05-14 09:09:05 +0530

    "update"

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/updated_resume2.00.pdf";
+import pdf from "../../Assets/../Assets/ResumeUpdated.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import Tilt from "react-parallax-tilt"
